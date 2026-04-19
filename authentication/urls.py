e611934f80469df1a3f5ac72289cac4f7eca3ec4
Ella Udotong <94646178+Shecodes-py@users.noreply.github.com>
2026-04-19 04:50:44 +0100

    Auth views

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,16 +1,19 @@
 from django.urls import path
-from . import views
+from .views import *
 
 # write your urls here
 urlpatterns = [
     # Auth
-    path("register/", views.RegisterView.as_view(), name="register"),
-    path("login/", views.LoginView.as_view(), name="login"),
-    path("token/refresh/", views.TokenRefreshView.as_view(), name="token-refresh"),
-    path("logout/", views.LogoutView.as_view(), name="logout"),
-    path("change-password/", views.ChangePasswordView.as_view(), name="change-password"),
+    path("register/", RegisterView.as_view(), name="register"),
+    path("login/", LoginView.as_view(), name="login"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+    path("change-password/", ChangePasswordView.as_view(), name="change-password"),
 
     # User
-    path("profile/", views.ProfileView.as_view(), name="profile"),
-    path("dashboard/", views.DashboardView.as_view(), name="dashboard"),
+    path("profile/", ProfileView.as_view(), name="profile"),
+    path("dashboard/", DashboardView.as_view(), name="dashboard"),
+
+    # blank
+    path("", index, name="home"),
 ]
