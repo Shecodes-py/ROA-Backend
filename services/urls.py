0e023775cef527cb613484211fb706656c9f70b6
Ella Udotong <udotongpeace@gmail.com>
2026-04-22 18:11:31 +0100

    Update urls.py

--- services/urls.py
+++ services/urls.py
@@ -7,10 +7,17 @@ app_name = 'bookings'
 urlpatterns = [
    path("health_check/", home, name="health_check"),
     
-    # Main booking page
-    path('booking', BookingViewSet.as_view({'get': 'list'}), name='booking_page'),
+    # Add-ons (Read-only)
     path('Addon', AdditionalServiceViewSet.as_view({'get': 'list'}), name='additional_services_list'),
     path('Addon/<int:pk>/', AdditionalServiceViewSet.as_view({'get': 'retrieve'}), name='additional_service_detail'),
+    
+    # Booking Options (For dropdowns)
     path('booking-options', booking_options, name='booking_options'),
-   
-]
+    
+    # Main booking page
+    #path('booking', BookingViewSet.as_view({'get': 'list'}), name='booking_page'),
+    path('bookings/', BookingViewSet.as_view({'get': 'list', 'post': 'create'}), name='booking_list_create'),
+    path('bookings/<int:pk>/', BookingViewSet.as_view({'get': 'retrieve', 'put': 'update', 'patch': 'partial_update'}), name='booking_detail'),
+    path('bookings/<int:pk>/cancel/', BookingViewSet.as_view({'post': 'cancel'}), name='booking_cancel'),
+ ]
+
